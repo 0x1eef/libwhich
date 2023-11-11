9ebfdf452e2ed6f96c1e4539c81f1c0003a8583f
0x1eef <0x1eef@protonmail.com>
2023-11-11 11:01:07 -0300

    Fix write-after-free
    Fix by creating a copy of the read-only string returned by getenv.

--- src/which.c
+++ src/which.c
@@ -20,10 +20,11 @@ char*
 which(const char *progname)
 {
   char *penv, *path;
-  penv = getenv("PATH");
+  penv = strdup(getenv("PATH"));
   if (penv == NULL) {
     return NULL;
   } else if (progname == NULL) {
+    free(penv);
     return NULL;
   } else {
     path = strtok(penv, COLON);
@@ -31,14 +32,17 @@ which(const char *progname)
       char *exe;
       exe = join(path, progname);
       if (exe == NULL) {
+        free(penv);
         return NULL;
       } else if (is_executable(exe)) {
+        free(penv);
         return exe;
       } else {
         free(exe);
       }
       path = strtok(NULL, COLON);
     }
+    free(penv);
     return NULL;
   }
 }
